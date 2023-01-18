27011a438c33338c24a86d737dd9646e4e5ce22a
Javier Razo <javier.razo1221@gmail.com>
2023-01-18 08:31:07 -0700

    fixed directory issues

--- src/contacts.java
+++ src/contacts.java
@@ -2,15 +2,22 @@
 // Contact: name, number, email
 // Database: multiple contacts
 
+import java.net.ContentHandlerFactory;
 import java.util.ArrayList;
 
 class ContactsDatabase {
     public static void main(String[] args) {
         Database friends = new Database("Friends");
         friends.addContact(new Contact("Bill", "bill@gmail.com", "546023234"));
-        friends.addContact(new Contact("Anna", "anna@gmnail.com", "3842348586"));
+        friends.addContact(new Contact("Anna", "anna@gmail.com", "3842348586"));
         friends.addContact(new Contact("Giovanni", "giovanni@yahoo.com", "1458658686"));
+
         System.out.println(friends);
+        System.out.println();
+
+        System.out.println(friends.findByName("Anna"));
+        System.out.println(friends.findByEmail("bill@gmail.com"));
+        System.out.println(friends.findByNumber("1458658686"));
     }
 }
 
@@ -46,7 +53,32 @@ class Database {
         this.contacts.add(c);
     }
 
-    public String toString(){
+    public Contact findByName(String name) {
+        for (Contact c : this.contacts) {
+            if (c.getName().equals(name)) {
+                return c;
+            }
+        }
+        return null;
+    }
+    public Contact findByEmail(String email) {
+        for (Contact c : this.contacts) {
+            if (c.getEmail().equals(email)) {
+                return c;
+            }
+        }
+        return null;
+    }
+    public Contact findByNumber(String number) {
+        for (Contact c : this.contacts) {
+            if (c.getNumber().equals(number)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public String toString() {
         String s = "";
         s += this.name;
         for (Contact c : this.contacts){
